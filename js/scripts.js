4dd62e27bf7ebf80fbda605dba77aeae90aa41c3
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-07 12:53:17 -0700

    add a second survey pagge to try to find a different way to get a result in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -23,5 +23,42 @@ $(document).ready(function() {
     }
   });
 
+  //this function only works with survey 2
+  $(".form2").submit(function(event) {
+    event.preventDefault();
+    //this hides the previous answer
+    $(".hidden").hide();
+    $(".contentBox").show();
+    // an array
+    var answers = [0,0,0,0,0];
+    //total stores the total from the values that the user inputted by answering questions
+    answers[0] = $("input:radio[name=question1]:checked").val();
+    answers[1] = $("input:radio[name=question2]:checked").val();
+    answers[2] = $("input:radio[name=question3]:checked").val();
+    answers[3] = $("input:radio[name=question4]:checked").val();
+    answers[4] = $("input:radio[name=question5]:checked").val();
+    // these if statements determine what language the index.html should show by unhiding the language related to the id based on which range the value in total falls into
+    var result;
+    if (answers[0] === answers[1] || answers[0] === answers[2] || answers[0] === answers[3] || answers[0] === answers[4]) {
+      result = answers[0];
+    } else if (answers[1] === answers[2] || answers[1] === answers[3] || answers[1] === answers[4]) {
+      result = answers[1];
+    } else if (answers[2] === answers[3] || answers[2] === answers[4]) {
+      result = answers[2];
+    } else {
+      result = answers[3];
+    }
 
+    if (result === answers[0] && result === answers[1] || result === answers[0] && result === answers[2] || result === answers[0] && result === answers[3] || result === answers[0] && result === answers[4]) {
+      alert(result);
+    } else if (result === answers[1] && result === answers[2] || result === answers[1] && result === answers[3] || result === answers[1] && result === answers[4]) {
+      alert(result);
+    } else if (result === answers[2] && result === answers[3] || result === answers[2] && result === answers[4]) {
+      alert(result);
+    } else if (result === answers[3] && result === answers[4]) {
+      alert(result);
+    } else {
+      alert(result);
+    }
+  });
 });
